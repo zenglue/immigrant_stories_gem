1d7ce9c75f881589d3b8faa4000975d55f3d1a1f
Keo  Bun <keonilrath@gmail.com>
2016-09-28 03:34:46 +0000

    environment.rb added, #initialize updated

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/scraper.rb'
+require_relative '../lib/immigrant_stories.rb'
--- lib/immigrant_stories.rb
+++ lib/immigrant_stories.rb
@@ -1,7 +1,8 @@
 class Immigrants
-  
+
   attr_accessor :name, :bio, :ethnicity, :world_region, :language, :profile_video, :video_transcript, :profile_url
 
-  def initialize
+  def initialize(name=nil, profile_url=nil)
     @name = name
+    @profile_url = profile_url
   end

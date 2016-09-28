2924f8a008a97f6830ad20d72af9dc691097669c
Keo  Bun <keonilrath@gmail.com>
2016-09-28 02:12:47 +0000

    scraper.rb set up for pry

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,12 @@ class Scraper
     Nokogiri::HTML(open("http://immigrants.mndigital.org/exhibits/show/immigrantstories-exhibit/page01"))
     binding.pry
   end
+
+  def scrape_immigrant_profiles
+#    doc = Nokogiri::HTML(open("http://immigrants.mndigital.org/exhibits/show/immigrantstories-exhibit/item/619"))
+  end
 end
 
 Scraper.new.get_page
+
+#names: doc.css("p").text

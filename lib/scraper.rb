32175b9a6028586557476313627770149a2cc934
Keo  Bun <keonilrath@gmail.com>
2016-09-28 05:32:43 +0000

    scrape_dreamer_profiles up updates

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,19 +29,11 @@ class Scraper
     profile_video = profile_file.css("[@id='playlist'] li[1] a").attribute("href").value
     video_transcript = profile_url.css("[@id='item-transcription'] div").text
 
+    dreamer_attributes[:bio] = bio
+    dreamer_attributes[:ethnicity] = ethnicity
+    dreamer_attributes[:region] = region
+    dreamer_attributes[:language] = language
+    dreamer_attributes[:profile_video] = profile_video
+    dreamer_attributes[:video_transcript] = video_transcript
   end
-
-
-
-
 end
-
-#names: doc.css("p").text
-#profile_url: doc.css("a.exhibit-item-link").attribute("href").value
-
-#profile_video: profile_file.css("[@id='playlist'] li[1] a").attribute("href").value
-#bio: profile_url.css("[@id='dublin-core-description'] div").text
-#ethnicity: profile_url.css("[@id='item-type-ethnicity'] div a").text
-#region: profile_url.css("[@id='item-type-world-region'] div a").text
-#language: profile_url.css("[@id='dublin-core-language'] div a").text
-##video_transcript: doc.css("[@id='item-transcription'] div").text
